01ba81888b2c5dd792e908fec1afc821e7ea2ff4
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-08-03 16:18:11 -0600

    create json API route

--- src/views.py
+++ src/views.py
@@ -1,5 +1,5 @@
 from src import app
-from flask import render_template
+from flask import render_template, request, jsonify, make_response
 from datetime import datetime
 from src.template_filters import clean_date
 
@@ -26,3 +26,24 @@ def jinja():
                            clean_date=clean_date,
                            my_html=my_html,
                            suspicious=suspicious)
+    
+@app.route("/json", methods=["POST"])
+def json():
+    if request.is_json:
+        
+        req = request.get_json()
+        
+        response = {
+            "message": "JSON received!",
+            "name": req.get("name")
+        }
+        
+        res = make_response(jsonify(response), 200)
+        
+        return res
+    else:
+        n_response = {
+            "message": "No JSON received"
+        }
+        res = make_response(jsonify(n_response), 400)
+        return res
