cb36d0167b7bcf6961468c3a8c117be8070e4d8e
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-08-04 21:17:07 -0600

    add dynamic message flashing

--- src/views.py
+++ src/views.py
@@ -1,7 +1,7 @@
 from src import app
 from flask import (render_template, redirect, request, 
                    jsonify, make_response, send_from_directory, abort,
-                   make_response, session, url_for)
+                   make_response, session, url_for, flash)
 from datetime import datetime
 from src.template_filters import clean_date
 import os
@@ -254,3 +254,24 @@ def sign_out():
     session.pop("USERNAME", None)
     
     return redirect(url_for("sign-in"))
+
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+    
+    if request.method == "POST":
+        
+        req = request.form
+        
+        username = req.get("username")
+        email = req.get("email")
+        password = req.get("password")
+        
+        if not len(password) >= 19:
+            flash("Password must be at least 10 characters in length", category="danger")
+            return redirect(request.url)
+        
+        flash("account created", category="success")
+        return redirect(request.url)
+    
+    return render_template("public/sign-up.html")
